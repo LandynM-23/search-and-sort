acc331b805e10253b99306a3df161a0bca18e0c4
LandynM-23 <112985822+LandynM-23@users.noreply.github.com>
2023-08-31 16:27:56 -0700

    Insertion Sort Created
    - Created basic insertion sort algorithm
    
    - Updated and/or removed comments

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -3,31 +3,29 @@ package sort;
 import java.util.Arrays;
 
 /**
- * Write a description of class Insertion here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * basic array insertion sort in Java
+ * @author Landyn
+ * @version 1.1.0
  */
 public class Insertion
 {
-  
-  /**
-   * Returns the sorted array
-   */
   public static int[] sort(int[] arr) {
-    // Your algorithm goes here!
-
+    for (int i = 1; i < arr.length; i++) {
+      for (int j = 1; j > 0 && arr[j] < arr[j - 1]; j--) {
+        int tempVar = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = tempVar;
+      }
+    }
+    return arr;
   }
-  
   public static void main(String[] args) {
     int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
       31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
       73,54,51,25,33,20,52,79,97,70,54,63,49};    
     
-    // Test the sort
     testSort(sort(arr));
   }
-  
   public static void testSort(int[] arr) {
     for (int i=0; i<arr.length-1; i++) {
       if (arr[i] > arr[i+1]) {
@@ -38,5 +36,4 @@ public class Insertion
     }
     System.out.println("SUCCESS!");
   }
-
 }
